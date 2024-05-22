4873a3518da404e40cc17be1e79466f766d1995a
lchouvil <luka.chouville@laposte.net>
2024-05-22 15:33:27 +0200

    develop/project-struct

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// pages/index.js
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>Explore my projects, skills, and experiences.</p>
+    </div>
+  );
+}
